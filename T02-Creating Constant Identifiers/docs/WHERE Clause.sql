649dbf2bea6a345fcc00a9d7b30162f4e6ac4709
Re9iNee <attarzadeh76@gmaila.com>
2021-03-24 13:13:01 +0430

    Adding Task 2 Method 1 SQL Query and NodeJs Code + how to use it

---
+++ T02-Creating Constant Identifiers/docs/WHERE Clause.sql
@@ -0,0 +1,5 @@
+SELECT TOP (1000) [CommonBaseTypeId]
+      ,[BaseTypeTitle]
+      ,[BaseTypeCode]
+  FROM [SabkadV01].[dbo].[tblCommonBaseType]
+  WHERE 1=1 AND BaseTypeCode=23 AND BaseTypeTitle='Asghar' AND CommonBaseTypeId=2
---
+++ app.js
@@ -0,0 +1,30 @@
+const sql = require("mssql");
+const config = {
+    user: "sa",
+    password: "Rainbow78951",
+    server: "localhost",
+    database: "SabkadV01",
+}
+const pool = new sql.ConnectionPool(config);
+const poolConnect = pool.connect();
+pool.on("error", err => {
+    console.log("Could not Connect to the Database", err);
+})
+const {ws_loadBaseType, ws_createBaseType} = require("./T02-Creating Constant Identifiers/commonBaseMethods");
+
+
+/* Examples: Task 02. Method 01 */
+// ws_loadBaseType({pool, poolConnect}, {BaseTypeCode: 23, BaseTypeTitle: "Asghar", CommonBaseTypeId: 2});
+// Usuage: 
+// (async () => {
+//     const result =await ws_loadBaseType({pool, poolConnect}, {BaseTypeCode: null, BaseTypeTitle: null, CommonBaseTypeId: null});
+//     console.dir(result)
+// })();
+
+
+
+
+
+
+
+// ws_createBaseType();
