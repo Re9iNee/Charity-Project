035c9215dfdb7f1b442f9e874ca573f39607f0da
Re9iNee <attarzadeh76@gmail.com>
2021-03-27 20:00:37 +0430

    wrote task 3 - method 01 and 02 / loading and creating commonBaseData table

---
+++ T03 - BaseInfo Services - Constant Values Task/constantValues.js
@@ -0,0 +1,52 @@
+const {
+    normalizeQueryString
+} = require("../others/commonModules");
+
+exports.ws_loadBaseValue = async (connection, filters, resultLimit = 1000) => {
+    const {
+        pool,
+        poolConnect
+    } = connection;
+    // ensures that the pool has been created
+    await poolConnect;
+    let queryString = `SELECT TOP (${resultLimit}) [CommonBaseDataId]
+    ,[CommonBaseTypeId]
+    ,[BaseValue]
+    ,[BaseCode]
+    FROM [SabkadV01].[dbo].[tblCommonBaseData]
+    WHERE 1=1`;
+    queryString = normalizeQueryString(queryString, filters);
+    try {
+        const request = pool.request();
+        const result = await request.query(queryString);
+        console.dir(result);
+        return result;
+    } catch (err) {
+        console.error("SQL error: ", err);
+    }
+}
+
+
+exports.ws_createBaseValue = async (connection, baseCode, baseValue, commonBaseTypeId) => {
+    const {
+        pool,
+        poolConnect
+    } = connection;
+    // ensures that the pool has been created
+    await poolConnect;
+    try {
+        // Select Scope Identity is for returning id of affected row(s)
+        let queryString = `INSERT INTO 
+        [SabkadV01].[dbo].[tblCommonBaseData]
+        (BaseCode, BaseValue, CommonBaseTypeId)
+        VALUES 
+        ('${baseCode}', '${baseValue}', ${commonBaseTypeId}); 
+        SELECT SCOPE_IDENTITY() AS CommonBaseDataId;`;
+        const request = pool.request();
+        const result = await request.query(queryString);
+        console.dir(result);
+        return result;
+    } catch (err) {
+        console.error("ws_createBaseValue error: ", err);
+    }
+}
--- app.js
+++ app.js
@@ -10,11 +10,14 @@ const poolConnect = pool.connect();
 pool.on("error", err => {
     console.log("Could not Connect to the Database", err);
 })
-const {
-    ws_loadBaseType,
-    ws_createBaseType
-} = require("./T02-Creating Constant Identifiers/commonBaseMethods");
 
+
+/*  TASK 2 */ 
+
+// const {
+//     ws_loadBaseType,
+//     ws_createBaseType
+// } = require("./T02-Creating Constant Identifiers/commonBaseMethods");
 
 /* Examples: Task 02. Method 01 */
 // ws_loadBaseType({pool, poolConnect}, {BaseTypeCode: 23, BaseTypeTitle: "Asghar", CommonBaseTypeId: 2});
@@ -29,11 +32,40 @@ const {
 
 
 // Exmaple: Task 02. Method 02
-(async () => {
-    const result = await ws_createBaseType({
-        pool,
-        poolConnect
-    }, 'Ame', '14')
-    console.log("Created Row has an Id Of, CommonBaseTypeId: ", result)
-})();
+// (async () => {
+//     const result = await ws_createBaseType({
+//         pool,
+//         poolConnect
+//     }, 'Ame', '14')
+//     console.log("Created Row has an Id Of, CommonBaseTypeId: ", result)
+// })();
 // ws_createBaseType({pool, poolConnect}, 'AsgharDAYI', 4);
+
+
+
+
+
+
+// Example: Task 03, Method 01
+/*  TASK 3 */
+const {
+    ws_loadBaseValue,
+    ws_createBaseValue
+} = require("./T03 - BaseInfo Services - Constant Values Task/constantValues");
+(async() => {
+        // Method 01
+    // const result = await ws_loadBaseValue({
+    //     pool,
+    //     poolConnect
+    // }, {
+    //     CommonBaseDataId: 1,
+    //     CommonBaseTypeId: '5',
+    //     BaseCode: '2',
+    //     BaseValue: 'dwad',
+    // })
+        // Method 02
+    // const result = await ws_createBaseValue({
+    //     pool,
+    //     poolConnect
+    // }, '23', '24', '4')
+})();
