38dc03c2e9ead129e4abed504202239a3ea0585f
Re9iNee <attarzadeh76@gmail.com>
2021-04-11 17:31:38 +0430

    Task 04 Method 01 charityAccounts - Application Crud - (Loading)

--- app.js
+++ app.js
@@ -146,17 +146,42 @@ app.route("/commonBaseData")
         res.send(result);
     });
 
+
+app.route("/charityAccounts")
+    .get(async (req, res) => {
+        let query = req.query;
+        // T04 - Method 01
+        // path: /charityAccounts/?CharityAccountId=1&BankId=6&BranchName=Ame&OwnerName=Reza
+        const result = await ws_loadCharityAccounts({
+            pool,
+            poolConnect
+        }, {
+            BankId: query.BankId,
+            BranchName: query.BranchName,
+            OwnerName: query.OwnerName,
+            CardNumber: query.CardNumber,
+            AccountNumber: query.AccountNumber,
+            AccountName: query.AccountName,
+            CharityAccountId: query.CharityAccountId,
+            BaseTypeCode: query.BaseTypeCode,
+        });
+        // Easier way to send request is to send query object itself, but when it comes to typo it throws an error
+        res.send(result)
+    });
+
 /* Task 04 */
 const {
     ws_loadCharityAccounts
 } = require("./T04 - Charity Accounts/charityAccounts");
 (async () => {
-    const result = await ws_loadCharityAccounts({
-        pool,
-        poolConnect
-    }, {
-        BankId: 6,
-        BaseTypeCode: '1'
-    })
-    console.log(result)
+    // Task 04 Method 01
+    // const result = await ws_loadCharityAccounts({
+    //     pool,
+    //     poolConnect
+    // }, {
+    //     BankId: 6,
+    //     BaseTypeCode: '1'
+    // })
+    // console.log(result)
+    
 })();
