c59c5b3e703aacefd41a8b9daf887c09fc6565a7
Re9iNee <attarzadeh76@gmail.com>
2021-05-03 19:30:19 +0430

    T09 - Method 02 - init commit - todos - details comments

--- PolWinno 0.2/src/services/cashAssistanceDetail.js
+++ PolWinno 0.2/src/services/cashAssistanceDetail.js
@@ -70,6 +70,34 @@ const ws_loadCashAssistanceDetail = async (connection, filters, customQuery = nu
 
 
 
+const ws_createCashAssistanceDetail = async (connection, details) => {
+    // details are the parameters sent for creating table
+    const {
+        AssignNeedyPlanId,
+        PlanId,
+        NeededPrice,
+        MinPrice,
+        Description
+    } = details;
+
+    // check inserted for not null values (check not null values)
+    // todo: if MinPrice was empty default will be "0"
+
+
+
+    // check for duplicates (check for unique columns)
+    // todo: assignNeedyPlanId and PlanId are unqiue values
+
+
+
+    // check for any column custome validations
+    // todo: MinPrice should be less or equal than NeededPrice
+
+
+    // put 'N' before any given string.
+}
+
+
 module.exports = {
     ws_loadCashAssistanceDetail,
 
