c8a01840225647c5ac593c74bfba145a831f45bb
Re9iNee <attarzadeh76@gmail.com>
2021-05-05 14:04:01 +0430

    tblCashAssistanceDetail - ALTER TABLE - Default MinPrice 0

--- PolWinno 0.2/doc/database - models - mssql/Queries/createTable-tblCashAssistanceDetail.sql
+++ PolWinno 0.2/doc/database - models - mssql/Queries/createTable-tblCashAssistanceDetail.sql
@@ -3,7 +3,7 @@ CREATE TABLE tblCashAssistanceDetail (
     AssignNeedyPlanId int,
     PlanId int NOT NULL,
     NeededPrice MONEY NOT NULL,
-    MinPrice MONEY,
+    MinPrice MONEY DEFAULT 0,
     Description TEXT,
     CONSTRAINT FK_CashAssistanceNeedyPlan FOREIGN KEY (AssignNeedyPlanId) REFERENCES [SabkadV01].[dbo].[tblAssignNeedyToPlans](AssignNeedyPlanId),
     CONSTRAINT FK_CashAssistancePlan FOREIGN KEY (PlanId) REFERENCES [SabkadV01].[dbo].[tblPlans](PlanId),
